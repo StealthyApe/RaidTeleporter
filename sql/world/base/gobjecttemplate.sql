d758f895e9a210b3e38b2911416c62e12515034a
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2023-09-29 18:04:07 +0800

    Added Default SQL Files

---
+++ sql/world/base/gobjecttemplate.sql
@@ -0,0 +1,6 @@
+REPLACE INTO `gameobject_template` (`entry`, `type`, `displayId`, `name`, `IconName`, `castBarCaption`, `unk1`, `size`, `Data0`, `Data1`, `Data2`, `Data3`, `Data4`, `Data5`, `Data6`, `Data7`, `Data8`,`Data9`, `Data10`, `Data11`, `Data12`, `Data13`, `Data14`, `Data15`, `Data16`, `Data17`, `Data18`, `Data19`, `Data20`, `Data21`, `Data22`, `Data23`, `AIName`, `ScriptName`, `VerifiedBuild`) VALUES 
+(902000, 10, 8632, 'MoltenCore Teleporter', '', '', '', 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 45000, 1, 0, 0, 0, '', 'RaidTeleporter', 0),
+(902001, 10, 8632, 'Black Wing Lair Teleporter', '', '', '', 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 45001, 1, 0, 0, 0, '', 'RaidTeleporter', 0),
+(902002, 10, 8632, 'Zul\'Gurub Teleporter', '', '', '', 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 45002, 1, 0, 0, 0, '', 'RaidTeleporter', 0),
+(902003, 10, 8632, 'Ruins of Ahn\'Qiraj Teleporter', '', '', '', 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 45003, 1, 0, 0, 0, '', 'RaidTeleporter', 0),
+(902004, 10, 8632, 'Temple of Ahn\'Qiraj Teleporter', '', '', '', 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 45004, 1, 0, 0, 0, '', 'RaidTeleporter', 0);
