8ac601201a6e7263177f28d15950c3c6c89d8bad
StealthyApe <59826804+StealthyApe@users.noreply.github.com>
2023-09-30 11:39:58 +0800

    Added support for skipping bosses

--- src/RaidTeleporter.cpp
+++ src/RaidTeleporter.cpp
@@ -1,4 +1,5 @@
 #include "RaidTeleporter.h"
+#include "iostream"
 
 enum RaidID {
 	MOLTEN_CORE = 409,
@@ -8,7 +9,7 @@ enum RaidID {
 	RUINS_OF_AHNQIRAJ = 509,
 	TEMPLE_OF_AHNQIRAJ = 531,
 	NAXXRAMAS_VANILLA,
-	KARAZHAN,
+	KARAZHAN = 532,
 	GRUULS_LAIR,
 	MAGTHERIDONS_LAIR,
 	THE_EYE,
@@ -38,30 +39,56 @@ bool RaidTeleporterScript::OnGossipHello(Player* player, GameObject* go) {
                 return true;
             }
             AddGossipItemFor(player, gosMenuId, 0, 0 , 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // always provide a teleport to the entrance
+            int skipper = 0; // total amount of values skipped
+            int timeSinceLastlegal = 0; // shift since last legal shift
+            bool targetskip = false; // to level speed farm know what is skipped
             switch(mode){
                 // iterator is equal to offset + 1 to stop a potential underflow error
                 // the value you saved to the gossip option is always iterator - offset +1
                 // the +1 is to offset the first teleport option always being the entrance and handles seperatetly
+                // skipper is used to handle neccesary skip when there's non boss information listed in bosses 
+                // by subtracting it from the sent value this maintains the sync
+                // potentially negates the need for offset
                 case PROGESSION:{
-                    for (int i = offset; i < teleLocations.size() + offset; i++) { // offset is needed as some boss lists don't start at 0
-			             if (instance->GetBossState(i) == DONE) {
-				            AddGossipItemFor(player, gosMenuId, i - offset + 1, i - offset + 1 , 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+                    for (int i = 0; i < teleLocations.size() + ToSkip.size(); i++) { // offset is needed as some boss lists don't start at 0
+                        if(CheckForSkip(i)){//adds one to skipper 
+                            skipper++;
+                        }
+			            else if (instance->GetBossState(i) == DONE) {
+				            AddGossipItemFor(player, gosMenuId, i + 1 - skipper, i + 1 - skipper , 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
 			            }
+                        std::cout << instance->GetBossState(i) << " " << i << '\n';
 		            }
                     break;
                 }
                 case SPEED:{
                     AddGossipItemFor(player, gosMenuId,1, 1, 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // creates the first entry then correctly works
-                    for (int i = offset + 1; i < teleLocations.size() + offset; i++) { // offset is needed as some boss lists don't start at 0
-			             if (instance->GetBossState(i-1) == DONE) { // checks if the previous boss is dead
-				            AddGossipItemFor(player, gosMenuId, i - offset + 1, i - offset + 1, 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+                    for (int i = 0 + 2; i < teleLocations.size() + ToSkip.size(); i++) { // offset is needed as some boss lists don't start at 0
+                        if(CheckForSkip(i)){//adds one to skipper 
+                            skipper++;
+                            timeSinceLastlegal++;
+                            targetskip = true;
+                        }
+			            else if (instance->GetBossState(i-1-timeSinceLastlegal) == DONE && targetskip) { // checks if the previous boss is dead
+				            AddGossipItemFor(player, gosMenuId, i + 1 - skipper, i + 1 - skipper, 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+                            targetskip = false;
+                            timeSinceLastlegal = 0;
 			            }
+                        else if (instance->GetBossState(i-1) == DONE && !targetskip) { // checks if the previous boss is dead
+				            AddGossipItemFor(player, gosMenuId, i + 1 - skipper, i + 1 - skipper, 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+			            }
+                         std::cout << instance->GetBossState(i) << " " << i << '\n';
 		            }
                     break;
                 }
                 case TARGET:{
-                    for (int i = offset; i < teleLocations.size() + offset; i++) { // offset is needed as some boss lists don't start at 0
-				            AddGossipItemFor(player, gosMenuId, i - offset + 1, i - offset + 1 , 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+                    for (int i = 0; i < teleLocations.size() + ToSkip.size(); i++) { // offset is needed as some boss lists don't start at 0
+                        if(CheckForSkip(i)){//adds one to skipper 
+                            skipper++;
+                        }
+                        else{
+				            AddGossipItemFor(player, gosMenuId, i + 1 - skipper, i + 1 - skipper , 0,sConfigMgr->GetOption<uint32>("RaidTeleporter.Cost",false)); // sends the index to select
+                        }
 		            }
                     break;
                 }
@@ -99,32 +126,42 @@ void RaidTeleporterScript::ZonePrep(GameObject* go) {
 	uint32 raid = go->GetMapId();
 	switch (raid) {
     case MOLTEN_CORE:{
-        offset = 0;
         teleLocations = RaidTeleLocations::MC;
         break;
     }
     case BLACKWING_LAIR:{
-        offset = 0;
         teleLocations = RaidTeleLocations::BWL;
         break;
     }
     case ZULGURUB:{
-        offset = 0;
         teleLocations = RaidTeleLocations::ZG;
         break;
     }
     case RUINS_OF_AHNQIRAJ:{
-        offset = 0;
         teleLocations = RaidTeleLocations::AQ20;
         break;
     }
     case TEMPLE_OF_AHNQIRAJ:{
-        offset = 1;
         teleLocations = RaidTeleLocations::AQ40;
+        ToSkip = {0};
+        break;
+    }
+    case KARAZHAN:{
+        offset = 0;
+        teleLocations = RaidTeleLocations::Kara;
+        ToSkip = {3};
         break;
     }
     }
 };
+bool RaidTeleporterScript::CheckForSkip(int toCheck){
+    for(int i = 0; i < ToSkip.size(); i++){
+        if(toCheck == ToSkip[i]){
+            return true;
+        }
+    }
+    return false;
+}
 
 void AddRaidTeleporterScripts(){
     new RaidTeleporterScript();
--- src/RaidTeleporter.h
+++ src/RaidTeleporter.h
@@ -14,11 +14,13 @@ private:
     int offset = 0;
     int totalBosses = 0;
     std::vector<RaidTeleLocations::Teleport_destination> teleLocations;
+    std::vector<int> ToSkip;
 
 public:
 	void ZonePrep(GameObject* go);
 	RaidTeleporterScript() : GameObjectScript("RaidTeleporter") { }
 	bool OnGossipHello(Player* player, GameObject* go) override;
 	bool OnGossipSelect(Player* player, GameObject* /*go*/, uint32 sender, uint32 action) override;
+    bool CheckForSkip(int toCheck);
 
 };
